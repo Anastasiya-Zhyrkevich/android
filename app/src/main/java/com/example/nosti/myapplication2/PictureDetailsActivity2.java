0fcd3dcd1dd077f89359f757b6d4505a121a0207
Anastasiya-Zhyrkevich <Anastasiya.Zhyrkevich@yandex.ru>
2016-03-13 11:51:59 +0300

    [master] Nav View fixed, non-working

--- app/src/main/java/com/example/nosti/myapplication2/PictureDetailsActivity2.java
+++ app/src/main/java/com/example/nosti/myapplication2/PictureDetailsActivity2.java
@@ -26,110 +26,111 @@ public class PictureDetailsActivity2 extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.photo_main2);
 
-        imageView = (ImageView) findViewById(R.id.imageView);
 
-        Bundle bundle = getIntent().getExtras();
-        Bitmap bitmap = BitmapUtils.getBitmap(getResources(),
-                bundle.getInt(PACKAGE_NAME + ".resourceId"));
-
-        imageView.setImageBitmap(bitmap);
-        setupZoom();
+//        imageView = (ImageView) findViewById(R.id.imageView);
+//
+//        Bundle bundle = getIntent().getExtras();
+//        Bitmap bitmap = BitmapUtils.getBitmap(getResources(),
+//                bundle.getInt(PACKAGE_NAME + ".resourceId"));
+//
+//        imageView.setImageBitmap(bitmap);
+//        setupZoom();
+//    }
+//
+//
+//    private void setupZoom() {
+//        scaleDetector = new ScaleGestureDetector(this, new ScaleListener());
+//    }
+//
+//    @Override
+//    public boolean onTouchEvent(MotionEvent ev) {
+//        scaleDetector.onTouchEvent(ev);
+//
+//        if (ev.getPointerCount() > 1) {
+//            return true;
+//        }
+//
+//        final int action = MotionEventCompat.getActionMasked(ev);
+//
+//        switch (action) {
+//            case MotionEvent.ACTION_DOWN: {
+//                final int pointerIndex = MotionEventCompat.getActionIndex(ev);
+//                final float x = MotionEventCompat.getX(ev, pointerIndex);
+//                final float y = MotionEventCompat.getY(ev, pointerIndex);
+//
+//                // Remember where we started (for dragging)
+//                lastTouchX = x;
+//                lastTouchY = y;
+//                // Save the ID of this pointer (for dragging)
+//                activePointerId = MotionEventCompat.getPointerId(ev, 0);
+//                break;
+//            }
+//
+//            case MotionEvent.ACTION_MOVE: {
+//                // Find the index of the active pointer and fetch its position
+//                final int pointerIndex =
+//                        MotionEventCompat.findPointerIndex(ev, activePointerId);
+//
+//                final float x = MotionEventCompat.getX(ev, pointerIndex);
+//                final float y = MotionEventCompat.getY(ev, pointerIndex);
+//
+//                // Calculate the distance moved
+//                final float dx = x - lastTouchX;
+//                final float dy = y - lastTouchY;
+//
+//                totalDiffX += dx;
+//                totalDiffY += dy;
+//
+//                imageView.setTranslationX(totalDiffX);
+//                imageView.setTranslationY(totalDiffY);
+//
+//                // Remember this touch position for the next move event
+//                lastTouchX = x;
+//                lastTouchY = y;
+//
+//                break;
+//            }
+//
+//            case MotionEvent.ACTION_UP: {
+//                activePointerId = INVALID_POINTER_ID;
+//                break;
+//            }
+//
+//            case MotionEvent.ACTION_CANCEL: {
+//                activePointerId = INVALID_POINTER_ID;
+//                break;
+//            }
+//
+//            case MotionEvent.ACTION_POINTER_UP: {
+//
+//                final int pointerIndex = MotionEventCompat.getActionIndex(ev);
+//                final int pointerId = MotionEventCompat.getPointerId(ev, pointerIndex);
+//
+//                if (pointerId == activePointerId) {
+//                    // This was our active pointer going up. Choose a new
+//                    // active pointer and adjust accordingly.
+//                    final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
+//                    lastTouchX = MotionEventCompat.getX(ev, newPointerIndex);
+//                    lastTouchY = MotionEventCompat.getY(ev, newPointerIndex);
+//                    activePointerId = MotionEventCompat.getPointerId(ev, newPointerIndex);
+//                }
+//                break;
+//            }
+//        }
+//        return true;
+//    }
+//
+//    private class ScaleListener
+//            extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+//        @Override
+//        public boolean onScale(ScaleGestureDetector detector) {
+//            scaleFactor *= detector.getScaleFactor();
+//            scaleFactor = Math.max(0.1f, Math.min(scaleFactor, 5.0f));
+//
+//            imageView.setScaleX(scaleFactor);
+//            imageView.setScaleY(scaleFactor);
+//            return true;
+//        }
+//    }
     }
-
-
-    private void setupZoom() {
-        scaleDetector = new ScaleGestureDetector(this, new ScaleListener());
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent ev) {
-        scaleDetector.onTouchEvent(ev);
-
-        if (ev.getPointerCount() > 1) {
-            return true;
-        }
-
-        final int action = MotionEventCompat.getActionMasked(ev);
-
-        switch (action) {
-            case MotionEvent.ACTION_DOWN: {
-                final int pointerIndex = MotionEventCompat.getActionIndex(ev);
-                final float x = MotionEventCompat.getX(ev, pointerIndex);
-                final float y = MotionEventCompat.getY(ev, pointerIndex);
-
-                // Remember where we started (for dragging)
-                lastTouchX = x;
-                lastTouchY = y;
-                // Save the ID of this pointer (for dragging)
-                activePointerId = MotionEventCompat.getPointerId(ev, 0);
-                break;
-            }
-
-            case MotionEvent.ACTION_MOVE: {
-                // Find the index of the active pointer and fetch its position
-                final int pointerIndex =
-                        MotionEventCompat.findPointerIndex(ev, activePointerId);
-
-                final float x = MotionEventCompat.getX(ev, pointerIndex);
-                final float y = MotionEventCompat.getY(ev, pointerIndex);
-
-                // Calculate the distance moved
-                final float dx = x - lastTouchX;
-                final float dy = y - lastTouchY;
-
-                totalDiffX += dx;
-                totalDiffY += dy;
-
-                imageView.setTranslationX(totalDiffX);
-                imageView.setTranslationY(totalDiffY);
-
-                // Remember this touch position for the next move event
-                lastTouchX = x;
-                lastTouchY = y;
-
-                break;
-            }
-
-            case MotionEvent.ACTION_UP: {
-                activePointerId = INVALID_POINTER_ID;
-                break;
-            }
-
-            case MotionEvent.ACTION_CANCEL: {
-                activePointerId = INVALID_POINTER_ID;
-                break;
-            }
-
-            case MotionEvent.ACTION_POINTER_UP: {
-
-                final int pointerIndex = MotionEventCompat.getActionIndex(ev);
-                final int pointerId = MotionEventCompat.getPointerId(ev, pointerIndex);
-
-                if (pointerId == activePointerId) {
-                    // This was our active pointer going up. Choose a new
-                    // active pointer and adjust accordingly.
-                    final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
-                    lastTouchX = MotionEventCompat.getX(ev, newPointerIndex);
-                    lastTouchY = MotionEventCompat.getY(ev, newPointerIndex);
-                    activePointerId = MotionEventCompat.getPointerId(ev, newPointerIndex);
-                }
-                break;
-            }
-        }
-        return true;
-    }
-
-    private class ScaleListener
-            extends ScaleGestureDetector.SimpleOnScaleGestureListener {
-        @Override
-        public boolean onScale(ScaleGestureDetector detector) {
-            scaleFactor *= detector.getScaleFactor();
-            scaleFactor = Math.max(0.1f, Math.min(scaleFactor, 5.0f));
-
-            imageView.setScaleX(scaleFactor);
-            imageView.setScaleY(scaleFactor);
-            return true;
-        }
-    }
-
 }
